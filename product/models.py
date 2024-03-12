f4ce486ab1fd51ca286739545474d143279064c4
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-03-12 03:05:37 +0200

    Add product app

---
+++ product/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = 'categories'
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+	description = models.CharField(max_length=250, default='', blank=True, null=True)
+	image = models.ImageField(upload_to='uploads/product/')
+	# Add Sale Stuff
+	is_sale = models.BooleanField(default=False)
+	sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+	def __str__(self):
+		return self.name
