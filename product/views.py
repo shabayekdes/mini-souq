3d9c1dd18dd63b5e57f7da273cfbbd740452ec61
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-07-11 09:22:15 +0300

    Update product details

--- product/views.py
+++ product/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Q
 
 # Create your views here.
 def product_details(request, id=None, *args, **kwargs):
     product = get_object_or_404(Product, id=id)
-    return render(request, 'product/details.html', {"product": product})
+    related_products = Product.objects.filter(
+        Q(category=product.category) | Q(brand=product.brand)
+    ).exclude(id=id)[:5] 
+
+    return render(request, 'product/details.html', {
+        "product": product,
+        "related_products": related_products
+        })
